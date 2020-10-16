78c99a5f8f7c4cf515ccbeba959bef0756affb40
clonwu <72661246+clonwu@users.noreply.github.com>
2020-10-17 02:02:56 +0800

    Initial commit

---
+++ static/js/tj.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">document.write(unescape("%3Cspan id='cnzz_stat_icon_1278861564'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1278861564' type='text/javascript'%3E%3C/script%3E"));</script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e10896006c9ef7e200298bd3d4f79a66";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
